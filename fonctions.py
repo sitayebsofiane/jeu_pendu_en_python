17ff847d1d99dddd2cbc89a633e84d19b92af740
sitayebsofiane <sitayebsofiane51@gmail.com>
2019-11-21 20:11:51 +0100

    utilisation fichier pour enrigistré le dernier joueur

--- fonctions.py
+++ fonctions.py
@@ -1,3 +1,14 @@
+class Player:
+    def __init__(self,name,score):
+        self.name=name
+        self.score=score
+    def add_score(self,new_score):
+        self.score+=new_score;
+        return self.score
+    def getName(self):
+        return self.name
+    def getScore(self):
+        return self.score
 #fonction to check name
 def name():
     name=input("entré votre nom ")
@@ -8,46 +19,62 @@ def name():
 def mots():
     import random
     liste_mot=["bruno","thomas","kamel","farid","sofiane","fally","emira","nour"]
-    return random.choice(liste_mot)
+    choix=random.choice(liste_mot)
+    return choix
 #fonction to check enter must be letter a to z
-def check_enter(lettre):
+def letrre_choise():
     import re
-    return  bool(re.match(r"^[a-z]$",lettre))
+    lettre=input("entrez une lettre ")
+    while not bool(re.match(r"^[a-z]$",lettre.lower())):
+         lettre=input("entrez une lettre ")
+    return lettre
 
 #fonction of play
 def part():
-    #genrate random word to ze liste
-    mot=mots()
-    nbr_coup=0
-    lettre=""
+    #genrate random word to ze liste word and transform it in liste of lettre
+    mot=[x for x in mots()[0:]]
     affichage=["*" for i in range(len(mot))]
-    index=0
-    while nbr_coup < 8 and index<len(mot):
-        lettre=input("entrez une lettre ")
-        #i check his enter if its letter
-        if check_enter(lettre):
-            if(lettre==mot[index]):
-                affichage[index]=lettre
-                index+=1
-                if(index!=len(mot)):
-                    print("".join(affichage)," bravo il vous reste ",8-nbr_coup,"coups")
-            else:
-                nbr_coup+=1
-                if 8-nbr_coup>0:
-                    print("".join(affichage),"attention il vous reste",8-nbr_coup,"coups")
-    if(nbr_coup==8):
+    tours=0
+    find=False
+    while tours<8 and affichage !=mot:
+        lettre=letrre_choise()
+        for i in range(len(mot)):
+            if lettre==mot[i] and affichage[i]=="*":
+                affichage[i]=lettre
+                print("".join(affichage)," bravo il vous reste ",8-tours,"coups")
+                find=True
+                break
+        if not find:
+            tours+=1
+            print("".join(affichage),"attention il vous reste",8-tours,"coups")
+        find=False
+    if tours==8 and affichage !=mot:
         print("vous avez perdu !")
     else:
-        print("bravo vous avez gagnez ",8-nbr_coup,"points")
-    return 8-nbr_coup
+        print("bravo vous avez gagnez ",8-tours,"points")
+    return 8-tours
 # i call fonction to part in play
 def play():
+    import pickle,os
     nom=name()
     print("bienvenu",nom)
-    score=0
+    add=True
+    with open("data","rb") as fic:
+        record=pickle.Unpickler(fic)
+        player=record.load()
+        if player.getName()==nom:
+            score=player.getScore()
+            add=False           
+    if add:
+        score=0
     while input("taper oui si vous voulez jouer ").lower()=="oui":
         score+=part()
         print("votre score est ",score)
+    if add:
+        player=Player(nom,score)
+        with open("data","wb") as fic:
+            record=pickle.Pickler(fic)
+            record.dump(player)
 
 
 
